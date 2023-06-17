5f60608b6f39103c313657954c5ec5fec13eb092
SanLeo461 <sanleo461461@gmail.com>
2023-06-17 22:44:55 +1000

    init contracts

--- src/ClickCreatePrepay.sol
+++ src/ClickCreatePrepay.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
+contract ClickCreatePrepay is Ownable2Step {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
-    function increment() public {
-        number++;
-    }
 }
